fcc6ec98b17d03f889779a2b3a64143950fd6d32
201631053 <hubot@githhub@com>
2018-04-04 15:24:46 +0900

    add play

--- Alien ImageTimer/ViewController.swift
+++ Alien ImageTimer/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 1 //사진의 인덱스 값증가
+    var myTimer = Timer()
+    var play = 0
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var Index: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 첫번째 이미지를 로드함 로로로롤로롤 나연 ~~~~!
+        Index.text = String(counter)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btStart(_ sender: Any) {
+        if play == 0 {
+            myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+            play = 1
+        } else {
+            myTimer.invalidate()
+            play = 0
+            
+        }
+    }
+    
+
+    @objc func doAnimation() {
+        if counter == 5 {
+            counter = 1
+        } else {
+            counter = counter + 1
+        }
+        myImageView.image = UIImage(named: "frame\(counter).png")
+        Index.text = String(counter)
+    }
 
 }
-
